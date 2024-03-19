5db0c06e5e3adf82dd68d445de41866eafc450ef
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-03-19 09:03:49 +0200

    add deleteOrder controller

--- src/routes/api/orders.ts
+++ src/routes/api/orders.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getAll } from '../../controllers/orders';
+import { deleteById, getAll } from '../../controllers/orders';
 import { Endpoints } from '../../constants';
+import { isValidId } from '../../middlewares';
 
 const router = express.Router();
 
 router.get(Endpoints.root, getAll);
+router.delete(`/:${Endpoints.dynamicId}`, isValidId, deleteById);
 
 export default router;
